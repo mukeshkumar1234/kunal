ba8ce61c464f3c432779453083d34f7a02aa704e
kunalba <mr.kunalbatham07@gmail.com>
2019-04-14 13:50:34 +0530

    First in message second in declare variable

--- kunal/src/main/java/com/kunal/kunal/First8.java
+++ kunal/src/main/java/com/kunal/kunal/First8.java
@@ -1,5 +1,8 @@
 package com.kunal.kunal;
 
 public class First8 {
+	public static void main(String args[]) {
+	System.out.println("nnnn");
+	}
 
 }
---
+++ kunal/src/main/java/com/kunal/kunal/Second.java
@@ -0,0 +1,10 @@
+package com.kunal.kunal;
+
+public class Second {
+	public static void  main(String args[]) {
+		int a=10;
+		System.out.println(a);
+		
+	} 
+
+}
